cb8b374208f8c6579e1a8da1834d3fc58c3cec7b
MrlKsglu <meralkuss@gmail.com>
2024-07-16 16:42:45 +0300

    Update Odev-15.js
    sort metoduyla ödev yapıldı.

--- Odev-15.js
+++ Odev-15.js
@@ -7,3 +7,22 @@
 
 Örnek Dizi: [3, 5, 7, 2, 8, 6]
 */
+
+function enBuyukSayi(arr)
+{
+    let enBuyuk=arr.sort((a,b)=>b-a);
+    return enBuyuk[0];
+
+
+}
+
+function enKucukSayi(arr)
+{
+    let enKucuk=arr.sort((a,b)=>a-b);
+    return enKucuk[0];
+}
+
+let dizi=[3, 5, 7, 2, 8, 6];
+
+console.log(enBuyukSayi(dizi));
+console.log(enKucukSayi(dizi));
